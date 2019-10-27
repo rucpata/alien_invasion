316229ef50cd367ccf400b16895d3723020672f4
rucpata <rucinska.patrycja@gmail.com>
2019-10-27 16:17:51 +0100

    Przesunięcie obcych w prawo

--- alien.py
+++ alien.py
@@ -21,6 +21,11 @@ class Alien(Sprite):
         # Przechowywnaie dokładnego położenia obcego.
         self.x = float(self.rect.x)
 
+    def update(self):
+        """Przesunięcie obcego w prawo."""
+        self.x += self.ai_settings.alien_speed_factor
+        self.rect.x = self.x
+
     def blitme(self):
         """Wywietlenie obcego w jego aktualnym położeniu."""
         self.screen.blit(self.image, self.rect)
