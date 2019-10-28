92b899cbcdd23891e5f9df405e63349bebdd6c60
rucpata <rucinska.patrycja@gmail.com>
2019-10-28 22:10:42 +0100

    Obcy, który dociera do dolnej krawędzi ekranu

--- game_functions.py
+++ game_functions.py
@@ -147,6 +147,19 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     # Pauza
     sleep(0.5)
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """
+    Sprawdzenie, czy którykolwiek obcy dotarł do dolnej krawędzi
+    ekranu.
+    """
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+
+            # Tak samo jak w przypadku zderzenia statku z obcym.
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
+
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """
     Sprawdzenie, czy flota znajduje sie przy krawędzi ekranu, a następnie
@@ -154,6 +167,10 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
+
     # Wykrywanie kolizji między obcymi i statkiem.
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+
+    #Wyszukiwanie obcych docierających do dolnej krawędzi ekranu.
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
