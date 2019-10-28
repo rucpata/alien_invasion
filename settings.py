ad7eaf0acedfbd6fecabf44c1a1a0033ee95d037
rucpata <rucinska.patrycja@gmail.com>
2019-10-28 09:24:32 +0100

    modyfikacja pocisków

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings():
         self.ship_speed_factor = 1.5
         # Ustawienia dotyczące pocisku.
         self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
