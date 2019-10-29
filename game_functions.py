0c669adcd93c5ebcde384a2317aeed85f6b63f24
rucpata <rucinska.patrycja@gmail.com>
2019-10-29 20:19:24 +0100

    Dezaktywacja przycisku

--- game_functions.py
+++ game_functions.py
@@ -52,7 +52,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """ Rozpoczęcie nowej gry po kliknięciu przycisku Gra przez użytkownika."""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
         # Wyzerowanie danych statystycznych gry.
         stats.reset_stats()
         stats.game_active = True
