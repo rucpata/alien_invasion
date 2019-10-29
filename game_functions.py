18c14b1d637e023f1737bcd257b1b98b98bc3a6b
rucpata <rucinska.patrycja@gmail.com>
2019-10-29 17:53:11 +0100

    zerowanie gry

--- game_functions.py
+++ game_functions.py
@@ -37,7 +37,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_DOWN:
         ship.moving_down = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     ''' Reakcja na zdarzenia generowane przez klawiaturę i mysz. '''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -48,12 +48,22 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
             check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """ Rozpoczęcie nowej gry po kliknięciu przycisku Gra przez użytkownika."""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # Wyzerowanie danych statystycznych gry.
+        stats.reset_stats()
         stats.game_active = True
+
+        # Usunięcie zawartości list aliens i bullets.
+        aliens.empty()
+        bullets.empty()
+
+        # Utworzenie nowej floty i wyśrodkowanie statku
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     ''' Uaktualnienie obrazów na ekranie i przejście do nowego ekranu.'''
